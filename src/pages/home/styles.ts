10689a4d1c48eb5c5507d39c75e565179c6dd83a
Lucas-M01 <lucas.montenegro.n@outlook.com>
2023-10-24 19:58:17 -0300

    feat: New component ClaimUsernameForm

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -14,16 +14,23 @@ export const Hero = styled('div', {
     maxWidth: 480,
     padding: '0 $10',
     
-    [`${Heading}`]: {
-        
+    [`> ${Heading}`]: {
+        '@media(max-width: 600px)': {
+            fontSize: '$6xl'
+        }
     },
 
-    [`${Text}`]: {
+    [`> ${Text}`]: {
         marginTop: '$2',
         color: '$gray200'
     },
 })
 
 export const Preview = styled('div', {
+    paddingRight: '$8',
+    overflow: 'hidden',
 
+    '@media(max-width: 600px)': {
+        display: 'none'
+    }
 })
