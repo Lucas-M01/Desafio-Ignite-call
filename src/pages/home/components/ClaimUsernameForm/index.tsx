e523ca2a1ebeefaf09271488e419dda4dd12bdb6
Lucas-M01 <lucas.montenegro.n@outlook.com>
2023-10-26 21:05:13 -0300

    feat: validation with zod, visual structure of the register component

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,16 +1,56 @@
-import { Button, TextInput } from "@ignite-ui/react";
-import { Form } from "./styles";
-import { ArrowRight } from "phosphor-react";
+import { Button, TextInput, Text } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Mínimo de 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, { message: 'Apenas letras e hifens' })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  return (
-    <Form as="form">
-        <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" crossOrigin />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
-        <Button size="sm" type="submit">Reservar
-            <ArrowRight />
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handlePreRegister)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          crossOrigin
+          {...register('username')}
+        />
+
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
         </Button>
-    </Form>
-  );
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,14 +11,15 @@ export default function Home() {
       <Hero>
         <Heading size="4xl">Agendamento descomplicado</Heading>
         <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
         </Text>
 
         <ClaimUsernameForm />
       </Hero>
-      
+
       <Preview>
-        <Image 
+        <Image
           src={previewImage}
           height={400}
           quality={100}
